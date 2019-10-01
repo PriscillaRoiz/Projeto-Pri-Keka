dc9cad4068cc78506161c4558e87f55aa7f20ffb
PriscillaRoiz <priscillagarcia02@hotmail.com>
2019-10-01 11:36:23 -0300

    commit antes almoçø

--- js/script.js
+++ js/script.js
@@ -1,47 +1,40 @@
 const umaTarefa = document.querySelector(".addTarefa__button");
+const erro = document.querySelector(".meu-erro");
+const NovasTarefas= document.querySelector(".tabela")
 
-const erro = document.querySelector(".meu-erro")
 
 umaTarefa.addEventListener("click", function (evento) {
 
     let tarefa = document.getElementById('InputTarefa');
     let textoTarefa = tarefa.value;
 
-
-    const novaTarefa = document.createElement("p");
-    novaTarefa.textContent = tarefa;
-    novaTarefa.classList.add("tarefa")
-    console.log(textoTarefa);
-
-    if (textoTarefa === ""){
+    if (textoTarefa === "") {
 
         erro.textContent = "Digite uma nova tarefa."
-
+        tarefa.value = ""
     }
     else {
-     
-        let NewText = document.createElement("p");
+
+        let NewText = document.createElement("il");
 
         NewText.textContent = textoTarefa;
-        listaDeTarefas.appendChild(NewText);
-        NewText.classList.add("tarefas");
+        NovasTarefas.appendChild(NewText);
 
     }
-    tarefa.value = ""
 });
 
 
 const muitasTarefas = document.querySelectorAll(".tarefas")
 
-muitasTarefas.forEach(function(excluir){
-    excluir.addEventListener("click", function(evento)
-    {if (
-        excluir.classList.contains("excluir")){
-        excluir.classList.remove("excluir");
+muitasTarefas.forEach(function (excluir) {
+    excluir.addEventListener("click", function (evento) {
+        if (
+            excluir.classList.contains("excluir")) {
+            excluir.classList.remove("excluir");
 
-    } else {
-        excluir.classList.add("excluir");
-    }
+        } else {
+            excluir.classList.add("excluir");
+        }
 
     })
 });
